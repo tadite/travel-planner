43a9a4789cc17f85227af172afa530dea380e851
il.chudov <Graze74@gmail.com>
2017-11-30 14:31:53 +0300

    Example added

---
+++ src/main/java/edu/nc/travelplanner/controller/MainController.java
@@ -0,0 +1,38 @@
+package edu.nc.travelplanner.controller;
+
+import edu.nc.travelplanner.form.PersonForm;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MainController {
+
+    private String error;
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String helloPage(Model model){
+
+        PersonForm personForm = new PersonForm();
+        model.addAttribute("personForm", personForm);
+
+        return "base";
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public String helloPage(Model model,
+                            @ModelAttribute("personForm")PersonForm personForm){
+
+        String name = personForm.getName();
+        if (name.length() > 0 && name != null){
+            model.addAttribute("name", name);
+            return "hello";
+        }
+
+        error = "name field empty";
+        model.addAttribute("error", error);
+        return "base";
+    }
+}
--- src/test/java/edu/nc/travelplanner/TravelplannerApplicationTests.java
+++ src/test/java/edu/nc/travelplanner/TravelplannerApplicationTests.java
@@ -1,4 +1,4 @@
-package nc_edu.travelplanner;
+package edu.nc.travelplanner;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
