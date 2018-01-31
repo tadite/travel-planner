139f2248f657fe8dd8d3ea6c364426f6b7a6b99d
Andrey Nazarov <nazarov.a.dev@gmail.com>
2018-01-31 14:53:45 +0000

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
-INSERT INTO app_user VALUES (1, 'test1', 'pass1') ON CONFLICT DO NOTHING;
-INSERT INTO app_user VALUES (2, 'test2', 'pass2') ON CONFLICT DO NOTHING;
+INSERT INTO app_user VALUES (1, 'test1', 'pass1') ON CONFLICT (id) DO NOTHING;
+INSERT INTO app_user VALUES (2, 'test2', 'pass2') ON CONFLICT (id) DO NOTHING;
