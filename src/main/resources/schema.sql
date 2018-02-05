ff36eaa8ae6d09f45e06044cd672ca1e08646996
Nazarov Andrey <nazarov.a.dev@gmail.com>
2018-02-05 22:22:49 +0300

    fixed schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,4 +1,32 @@
-CREATE TABLE IF NOT EXISTS category
+CREATE TABLE IF NOT EXISTS type_of_residence
+(
+  type_of_residence_id bigserial NOT NULL,
+  description character varying(255),
+  name character varying(255),
+  CONSTRAINT type_of_residence_pkey PRIMARY KEY (type_of_residence_id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE type_of_residence
+  OWNER TO postgres;
+
+
+CREATE TABLE IF NOT EXISTS type_of_rest
+(
+  type_of_rest_id bigserial NOT NULL,
+  description character varying(255),
+  name character varying(255),
+  CONSTRAINT type_of_rest_pkey PRIMARY KEY (type_of_rest_id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE type_of_rest
+  OWNER TO postgres;
+  
+  
+ CREATE TABLE IF NOT EXISTS category
 (
   category_id bigserial NOT NULL,
   name character varying(255),
@@ -9,7 +37,205 @@ WITH (
   OIDS=FALSE
 );
 ALTER TABLE category
-  OWNER TO admin;
+  OWNER TO postgres;
+
+
+CREATE TABLE IF NOT EXISTS country
+(
+  country_id bigserial NOT NULL,
+  name character varying(255),
+  CONSTRAINT country_pkey PRIMARY KEY (country_id),
+  CONSTRAINT unique_country_name UNIQUE (name)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE country
+  OWNER TO postgres;
+
+
+CREATE TABLE IF NOT EXISTS city
+(
+  city_id bigserial NOT NULL,
+  name character varying(255),
+  country_id bigint,
+  CONSTRAINT city_pkey PRIMARY KEY (city_id),
+  CONSTRAINT fkrpd7j1p7yxr784adkx4pyepba FOREIGN KEY (country_id)
+      REFERENCES country (country_id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE city
+  OWNER TO postgres;
+
+
+CREATE TABLE IF NOT EXISTS option
+(
+  option_id bigserial NOT NULL,
+  name character varying(255),
+  CONSTRAINT option_pkey PRIMARY KEY (option_id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE option
+  OWNER TO postgres;
+
+
+CREATE TABLE IF NOT EXISTS client
+(
+  client_id bigserial NOT NULL,
+  age integer,
+  email character varying(255),
+  first_name character varying(255),
+  is_blocked boolean,
+  last_name character varying(255),
+  login character varying(255),
+  password character varying(255),
+  role character varying(255),
+  city_id bigint,
+  country_id bigint,
+  CONSTRAINT client_pkey PRIMARY KEY (client_id),
+  CONSTRAINT fkevdwlrxhbct07e6dighauj6er FOREIGN KEY (country_id)
+      REFERENCES country (country_id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT fkkb5fr3nx3qucrbme4wewcmwbf FOREIGN KEY (city_id)
+      REFERENCES city (city_id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE client
+  OWNER TO postgres;
+
+
+CREATE TABLE IF NOT EXISTS social_network
+(
+  social_network_id bigserial NOT NULL,
+  name character varying(255),
+  client_id bigint,
+  CONSTRAINT social_network_pkey PRIMARY KEY (social_network_id),
+  CONSTRAINT fkd8bsx801cbehoo2aces275bvv FOREIGN KEY (client_id)
+      REFERENCES client (client_id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE social_network
+  OWNER TO postgres;
+
+
+
+CREATE TABLE IF NOT EXISTS travel
+(
+  travel_id bigserial NOT NULL,
+  cost character varying(255),
+  description character varying(255),
+  name character varying(255),
+  number_of_days integer,
+  CONSTRAINT travel_pkey PRIMARY KEY (travel_id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE travel
+  OWNER TO postgres;
+
+
+
+
+CREATE TABLE IF NOT EXISTS option_for_travel
+(
+  option_for_travel_id bigserial NOT NULL,
+  description character varying(255),
+  option_id bigint,
+  travel_id bigint,
+  CONSTRAINT option_for_travel_pkey PRIMARY KEY (option_for_travel_id),
+  CONSTRAINT fk6t6aamghnonl4kndhsyq71rtj FOREIGN KEY (travel_id)
+      REFERENCES travel (travel_id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT fkryxdnceow0j3rh2apm5he3c4s FOREIGN KEY (option_id)
+      REFERENCES option (option_id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE option_for_travel
+  OWNER TO postgres;
+
+
+CREATE TABLE IF NOT EXISTS place_of_residence
+(
+  place_of_residence_id bigserial NOT NULL,
+  climate character varying(255),
+  cost_per_day character varying(255),
+  description character varying(255),
+  name character varying(255),
+  city_id bigint,
+  country_id bigint,
+  type_of_residence_id bigint,
+  CONSTRAINT place_of_residence_pkey PRIMARY KEY (place_of_residence_id),
+  CONSTRAINT fk12vprs2ycgxwoy9h9lxqe1ono FOREIGN KEY (type_of_residence_id)
+      REFERENCES type_of_residence (type_of_residence_id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT fkgtit24jkw7myucu0pqdp183f7 FOREIGN KEY (city_id)
+      REFERENCES city (city_id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT fkiqtxgvfuhgdgckvvokanpogue FOREIGN KEY (country_id)
+      REFERENCES country (country_id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE place_of_residence
+  OWNER TO postgres;
+
+CREATE TABLE IF NOT EXISTS travel_for_client
+(
+  travel_for_client_id bigserial NOT NULL,
+  description character varying(255),
+  client_id bigint,
+  travel_id bigint,
+  CONSTRAINT travel_for_client_pkey PRIMARY KEY (travel_for_client_id),
+  CONSTRAINT fkc44xsu6pft0n93nn828b48a98 FOREIGN KEY (travel_id)
+      REFERENCES travel (travel_id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT fkdpw333r08tcgrl215fp9wva6r FOREIGN KEY (client_id)
+      REFERENCES client (client_id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE travel_for_client
+  OWNER TO postgres;
+
+
+
+CREATE TABLE IF NOT EXISTS type_of_movement
+(
+  type_of_movement_id bigserial NOT NULL,
+  cost character varying(255),
+  description character varying(255),
+  duration character varying(255),
+  number_of_people integer,
+  category_id bigint,
+  CONSTRAINT type_of_movement_pkey PRIMARY KEY (type_of_movement_id),
+  CONSTRAINT fko4yaqfq43th00k0w05infn0py FOREIGN KEY (category_id)
+      REFERENCES category (category_id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE type_of_movement
+  OWNER TO postgres;
 
 
 CREATE TABLE IF NOT EXISTS check_point
@@ -40,66 +266,7 @@ WITH (
   OIDS=FALSE
 );
 ALTER TABLE check_point
-  OWNER TO admin;
-
-CREATE TABLE IF NOT EXISTS city
-(
-  city_id bigserial NOT NULL,
-  name character varying(255),
-  country_id bigint,
-  CONSTRAINT city_pkey PRIMARY KEY (city_id),
-  CONSTRAINT fkrpd7j1p7yxr784adkx4pyepba FOREIGN KEY (country_id)
-      REFERENCES country (country_id) MATCH SIMPLE
-      ON UPDATE NO ACTION ON DELETE NO ACTION
-)
-WITH (
-  OIDS=FALSE
-);
-ALTER TABLE city
-  OWNER TO admin;
-
-
-CREATE TABLE IF NOT EXISTS client
-(
-  client_id bigserial NOT NULL,
-  age integer,
-  email character varying(255),
-  first_name character varying(255),
-  is_blocked boolean,
-  last_name character varying(255),
-  login character varying(255),
-  password character varying(255),
-  role character varying(255),
-  city_id bigint,
-  country_id bigint,
-  CONSTRAINT client_pkey PRIMARY KEY (client_id),
-  CONSTRAINT fkevdwlrxhbct07e6dighauj6er FOREIGN KEY (country_id)
-      REFERENCES country (country_id) MATCH SIMPLE
-      ON UPDATE NO ACTION ON DELETE NO ACTION,
-  CONSTRAINT fkkb5fr3nx3qucrbme4wewcmwbf FOREIGN KEY (city_id)
-      REFERENCES city (city_id) MATCH SIMPLE
-      ON UPDATE NO ACTION ON DELETE NO ACTION
-)
-WITH (
-  OIDS=FALSE
-);
-ALTER TABLE client
-  OWNER TO admin;
-
-
-
-CREATE TABLE IF NOT EXISTS country
-(
-  country_id bigserial NOT NULL,
-  name character varying(255),
-  CONSTRAINT country_pkey PRIMARY KEY (country_id),
-  CONSTRAINT unique_country_name UNIQUE (name)
-)
-WITH (
-  OIDS=FALSE
-);
-ALTER TABLE country
-  OWNER TO admin;
+  OWNER TO postgres;
 
 
 CREATE TABLE IF NOT EXISTS excursion
@@ -126,173 +293,4 @@ WITH (
   OIDS=FALSE
 );
 ALTER TABLE excursion
-  OWNER TO admin;
-
-
-CREATE TABLE IF NOT EXISTS option
-(
-  option_id bigserial NOT NULL,
-  name character varying(255),
-  CONSTRAINT option_pkey PRIMARY KEY (option_id)
-)
-WITH (
-  OIDS=FALSE
-);
-ALTER TABLE option
-  OWNER TO admin;
-
-
-CREATE TABLE IF NOT EXISTS option_for_travel
-(
-  option_for_travel_id bigserial NOT NULL,
-  description character varying(255),
-  option_id bigint,
-  travel_id bigint,
-  CONSTRAINT option_for_travel_pkey PRIMARY KEY (option_for_travel_id),
-  CONSTRAINT fk6t6aamghnonl4kndhsyq71rtj FOREIGN KEY (travel_id)
-      REFERENCES travel (travel_id) MATCH SIMPLE
-      ON UPDATE NO ACTION ON DELETE NO ACTION,
-  CONSTRAINT fkryxdnceow0j3rh2apm5he3c4s FOREIGN KEY (option_id)
-      REFERENCES option (option_id) MATCH SIMPLE
-      ON UPDATE NO ACTION ON DELETE NO ACTION
-)
-WITH (
-  OIDS=FALSE
-);
-ALTER TABLE option_for_travel
-  OWNER TO admin;
-
-
-CREATE TABLE IF NOT EXISTS place_of_residence
-(
-  place_of_residence_id bigserial NOT NULL,
-  climate character varying(255),
-  cost_per_day character varying(255),
-  description character varying(255),
-  name character varying(255),
-  city_id bigint,
-  country_id bigint,
-  type_of_residence_id bigint,
-  CONSTRAINT place_of_residence_pkey PRIMARY KEY (place_of_residence_id),
-  CONSTRAINT fk12vprs2ycgxwoy9h9lxqe1ono FOREIGN KEY (type_of_residence_id)
-      REFERENCES type_of_residence (type_of_residence_id) MATCH SIMPLE
-      ON UPDATE NO ACTION ON DELETE NO ACTION,
-  CONSTRAINT fkgtit24jkw7myucu0pqdp183f7 FOREIGN KEY (city_id)
-      REFERENCES city (city_id) MATCH SIMPLE
-      ON UPDATE NO ACTION ON DELETE NO ACTION,
-  CONSTRAINT fkiqtxgvfuhgdgckvvokanpogue FOREIGN KEY (country_id)
-      REFERENCES country (country_id) MATCH SIMPLE
-      ON UPDATE NO ACTION ON DELETE NO ACTION
-)
-WITH (
-  OIDS=FALSE
-);
-ALTER TABLE place_of_residence
-  OWNER TO admin;
-
-
-
-CREATE TABLE IF NOT EXISTS social_network
-(
-  social_network_id bigserial NOT NULL,
-  name character varying(255),
-  client_id bigint,
-  CONSTRAINT social_network_pkey PRIMARY KEY (social_network_id),
-  CONSTRAINT fkd8bsx801cbehoo2aces275bvv FOREIGN KEY (client_id)
-      REFERENCES client (client_id) MATCH SIMPLE
-      ON UPDATE NO ACTION ON DELETE NO ACTION
-)
-WITH (
-  OIDS=FALSE
-);
-ALTER TABLE social_network
-  OWNER TO admin;
-
-
-
-CREATE TABLE IF NOT EXISTS travel
-(
-  travel_id bigserial NOT NULL,
-  cost character varying(255),
-  description character varying(255),
-  name character varying(255),
-  number_of_days integer,
-  CONSTRAINT travel_pkey PRIMARY KEY (travel_id)
-)
-WITH (
-  OIDS=FALSE
-);
-ALTER TABLE travel
-  OWNER TO admin;
-
-
-
-CREATE TABLE IF NOT EXISTS travel_for_client
-(
-  travel_for_client_id bigserial NOT NULL,
-  description character varying(255),
-  client_id bigint,
-  travel_id bigint,
-  CONSTRAINT travel_for_client_pkey PRIMARY KEY (travel_for_client_id),
-  CONSTRAINT fkc44xsu6pft0n93nn828b48a98 FOREIGN KEY (travel_id)
-      REFERENCES travel (travel_id) MATCH SIMPLE
-      ON UPDATE NO ACTION ON DELETE NO ACTION,
-  CONSTRAINT fkdpw333r08tcgrl215fp9wva6r FOREIGN KEY (client_id)
-      REFERENCES client (client_id) MATCH SIMPLE
-      ON UPDATE NO ACTION ON DELETE NO ACTION
-)
-WITH (
-  OIDS=FALSE
-);
-ALTER TABLE travel_for_client
-  OWNER TO admin;
-
-
-
-CREATE TABLE IF NOT EXISTS type_of_movement
-(
-  type_of_movement_id bigserial NOT NULL,
-  cost character varying(255),
-  description character varying(255),
-  duration character varying(255),
-  number_of_people integer,
-  category_id bigint,
-  CONSTRAINT type_of_movement_pkey PRIMARY KEY (type_of_movement_id),
-  CONSTRAINT fko4yaqfq43th00k0w05infn0py FOREIGN KEY (category_id)
-      REFERENCES category (category_id) MATCH SIMPLE
-      ON UPDATE NO ACTION ON DELETE NO ACTION
-)
-WITH (
-  OIDS=FALSE
-);
-ALTER TABLE type_of_movement
-  OWNER TO admin;
-
-
-
-CREATE TABLE IF NOT EXISTS type_of_residence
-(
-  type_of_residence_id bigserial NOT NULL,
-  description character varying(255),
-  name character varying(255),
-  CONSTRAINT type_of_residence_pkey PRIMARY KEY (type_of_residence_id)
-)
-WITH (
-  OIDS=FALSE
-);
-ALTER TABLE type_of_residence
-  OWNER TO admin;
-
-
-CREATE TABLE IF NOT EXISTS type_of_rest
-(
-  type_of_rest_id bigserial NOT NULL,
-  description character varying(255),
-  name character varying(255),
-  CONSTRAINT type_of_rest_pkey PRIMARY KEY (type_of_rest_id)
-)
-WITH (
-  OIDS=FALSE
-);
-ALTER TABLE type_of_rest
-  OWNER TO admin;
+  OWNER TO postgres;
