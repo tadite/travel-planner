11f7707653ae066f855ff58ef53662a81e94a147
Mariya <mariya.lukina7@gmail.com>
2018-03-15 13:40:04 +0300

    change questionaire

--- front_end/src/app/question/questions.component.ts
+++ front_end/src/app/question/questions.component.ts
@@ -43,7 +43,7 @@ states: string[] = ['shown', 'hidden', 'hidden', 'hidden'];
     questions: any;
     objectKeys = Object.keys;
     checks: {};
-      
+    mapDates = {};
     actionUrl: string = '/action';    
     
     login: string;
@@ -67,10 +67,12 @@ states: string[] = ['shown', 'hidden', 'hidden', 'hidden'];
 
     onSubmit(f: NgForm) {
         var map = {};
+       
         var tempForm = f.value;
         for(var key in tempForm){
-            map[tempForm[key]]= key;
-        }
+             map[tempForm[key]]= key;
+        }        
+
         for(var key in this.checks){
             if (key!=null)
                 map[key]= this.checks[key];
@@ -84,8 +86,8 @@ states: string[] = ['shown', 'hidden', 'hidden', 'hidden'];
         },
         error => {console.log(error);}
         );
-
     }
+    
 
     getNextActionView(){
         this.loading = true;
@@ -104,12 +106,18 @@ states: string[] = ['shown', 'hidden', 'hidden', 'hidden'];
         );
     }
 
-    putCheckId(id: any){
+    putCheckId(id: any) {
+        console.log('id: ' + id); 
+        console.log('checks: ' + this.checks); 
         if (this.checks[id]==='checked')
             this.checks[id]=null;
         else
             this.checks[id]='checked';
-        console.log(this.checks); 
+        console.log('checks: ' + this.checks); 
+    }
+
+    getCheckId(id: any){
+        this.checks[id]='checked';
     }
 
     logout(){
